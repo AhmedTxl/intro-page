2c523cc4900e45c01a84e3c027ff7ccf86f08503
AhmedTx <63393122+AhmedTxl@users.noreply.github.com>
2023-05-03 05:37:26 +0300

    Adding margin-top for the footer section

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -430,7 +430,7 @@
   <div id="overlayer" :class="active ? 'bg-black absolute opacity-70 top-0 bottom-0 left-0 w-[36%]' : 'opacity-100'" class="opacity-100"></div>
  
   <!-- Footer Contents -->
-  <footer class="text-black">
+  <footer class="text-black mt-2">
     Challenge by
     <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
     |
